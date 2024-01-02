0524657970c8ef4056507c621b86bfac374d1744
gsmbrest <136183780+gsmbrest@users.noreply.github.com>
2024-01-03 01:01:01 +0300

    refactoring full code

---
+++ script.js
@@ -0,0 +1,19 @@
+window.onscroll = function() {
+  scrollFunction();
+};
+
+function scrollFunction() {
+  let scrollToTopBtn = document.getElementById("scrollToTopBtn");
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    scrollToTopBtn.style.display = "block";
+  } else {
+    scrollToTopBtn.style.display = "none";
+  }
+}
+
+function scrollToTop() {
+  window.scrollTo({
+    top: 0,
+    behavior: 'smooth'
+  });
+}
